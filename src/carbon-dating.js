fd8361c0e365b39637d9a3a9fc922227ddc4333e
Kirill Litvin <litvin.kirill.m@mail.ru>
2023-04-13 22:54:08 +0300

    add new files

---
+++ src/carbon-dating.js
@@ -0,0 +1,35 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+
+/**
+ * Determine the age of archeological find by using
+ * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
+ * 
+ * @param {String} sampleActivity string representation of current activity 
+ * @return {Number | Boolean} calculated age in years or false
+ * in case of incorrect sampleActivity
+ *
+ * @example
+ * 
+ * dateSample('1') => 22387
+ * dateSample('WOOT!') => false
+ *
+ */
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false;
+
+  let activity = parseFloat(sampleActivity);
+
+  if (isNaN(activity) || activity <= 0 || activity > MODERN_ACTIVITY) return false;
+
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let t = Math.log(MODERN_ACTIVITY / activity) / k;
+  
+  return Math.ceil(t);
+}
+
+module.exports = {
+  dateSample
+};
